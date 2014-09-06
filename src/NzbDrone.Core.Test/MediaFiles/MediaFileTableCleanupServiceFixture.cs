Sonarr/Sonarr

New: Show episode file file deletions in history and episode activity

src/NzbDrone.Core.Test/MediaFiles/MediaFileTableCleanupServiceFixture.cs
@@ -79,7 +79,7 @@ namespace NzbDrone.Core.Test.MediaFiles
 
             Subject.Execute(new CleanMediaFileDb(0));
 
-            Mocker.GetMock<IMediaFileService>().Verify(c => c.Delete(It.Is<EpisodeFile>(e => e.RelativePath == DELETED_PATH), false), Times.Exactly(2));
+            Mocker.GetMock<IMediaFileService>().Verify(c => c.Delete(It.Is<EpisodeFile>(e => e.RelativePath == DELETED_PATH), DeleteMediaFileReason.RemovedFromDisk), Times.Exactly(2));
         }
 
         [Test]
@@ -95,7 +95,7 @@ namespace NzbDrone.Core.Test.MediaFiles
 
             Subject.Execute(new CleanMediaFileDb(0));
 
-            Mocker.GetMock<IMediaFileService>().Verify(c => c.Delete(It.IsAny<EpisodeFile>(), false), Times.Exactly(10));
+            Mocker.GetMock<IMediaFileService>().Verify(c => c.Delete(It.IsAny<EpisodeFile>(), DeleteMediaFileReason.NoLinkedEpisodes), Times.Exactly(10));
         }
 
         [Test]

src/NzbDrone.Core.Test/TvTests/EpisodeProviderTests/HandleEpisodeFileDeletedFixture.cs
@@ -58,7 +58,7 @@ namespace NzbDrone.Core.Test.TvTests.EpisodeProviderTests
         {
             GivenSingleEpisodeFile();
 
-            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, false));
+            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, DeleteMediaFileReason.RemovedFromDisk));
 
             Mocker.GetMock<IEpisodeRepository>()
                 .Verify(v => v.Update(It.Is<Episode>(e => e.EpisodeFileId == 0)), Times.Once());
@@ -69,7 +69,7 @@ namespace NzbDrone.Core.Test.TvTests.EpisodeProviderTests
         {
             GivenMultiEpisodeFile();
 
-            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, false));
+            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, DeleteMediaFileReason.RemovedFromDisk));
 
             Mocker.GetMock<IEpisodeRepository>()
                 .Verify(v => v.Update(It.Is<Episode>(e => e.EpisodeFileId == 0)), Times.Exactly(2));
@@ -84,7 +84,7 @@ namespace NzbDrone.Core.Test.TvTests.EpisodeProviderTests
                   .SetupGet(s => s.AutoUnmonitorPreviouslyDownloadedEpisodes)
                   .Returns(true);
 
-            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, false));
+            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, DeleteMediaFileReason.RemovedFromDisk));
 
             Mocker.GetMock<IEpisodeRepository>()
                 .Verify(v => v.Update(It.Is<Episode>(e => e.Monitored == false)), Times.Once());
@@ -99,7 +99,7 @@ namespace NzbDrone.Core.Test.TvTests.EpisodeProviderTests
                   .SetupGet(s => s.AutoUnmonitorPreviouslyDownloadedEpisodes)
                   .Returns(false);
 
-            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, false));
+            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, DeleteMediaFileReason.Upgrade));
 
             Mocker.GetMock<IEpisodeRepository>()
                 .Verify(v => v.Update(It.Is<Episode>(e => e.Monitored == true)), Times.Once());
@@ -114,7 +114,7 @@ namespace NzbDrone.Core.Test.TvTests.EpisodeProviderTests
                   .SetupGet(s => s.AutoUnmonitorPreviouslyDownloadedEpisodes)
                   .Returns(true);
 
-            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, true));
+            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, DeleteMediaFileReason.Upgrade));
 
             Mocker.GetMock<IEpisodeRepository>()
                 .Verify(v => v.Update(It.Is<Episode>(e => e.Monitored == true)), Times.Once());

src/NzbDrone.Core/MediaFiles/MediaFileTableCleanupService.cs
@@ -47,14 +47,14 @@ namespace NzbDrone.Core.MediaFiles
                     if (!_diskProvider.FileExists(episodeFilePath))
                     {
                         _logger.Debug("File [{0}] no longer exists on disk, removing from db", episodeFilePath);
-                        _mediaFileService.Delete(episodeFile);
+                        _mediaFileService.Delete(episodeFile, DeleteMediaFileReason.RemovedFromDisk);
                         continue;
                     }
 
                     if (!episodes.Any(e => e.EpisodeFileId == episodeFile.Id))
                     {
                         _logger.Debug("File [{0}] is not assigned to any episodes, removing from db", episodeFilePath);
-                        _mediaFileService.Delete(episodeFile);
+                        _mediaFileService.Delete(episodeFile, DeleteMediaFileReason.NoLinkedEpisodes);
                         continue;
                     }
 

Sportarr/Sportarr

src/NzbDrone.Core.Test/MediaFiles/MediaFileTableCleanupServiceFixture.cs
@@ -79,7 +79,7 @@ namespace NzbDrone.Core.Test.MediaFiles
 
             Subject.Execute(new CleanMediaFileDb(0));
 
-            Mocker.GetMock<IMediaFileService>().Verify(c => c.Delete(It.Is<EpisodeFile>(e => e.RelativePath == DELETED_PATH), false), Times.Exactly(2));
+            Mocker.GetMock<IMediaFileService>().Verify(c => c.Delete(It.Is<EpisodeFile>(e => e.RelativePath == DELETED_PATH), DeleteMediaFileReason.MissingFromDisk), Times.Exactly(2));
         }
 
         [Test]
@@ -95,7 +95,7 @@ namespace NzbDrone.Core.Test.MediaFiles
 
             Subject.Execute(new CleanMediaFileDb(0));
 
-            Mocker.GetMock<IMediaFileService>().Verify(c => c.Delete(It.IsAny<EpisodeFile>(), false), Times.Exactly(10));
+            Mocker.GetMock<IMediaFileService>().Verify(c => c.Delete(It.IsAny<EpisodeFile>(), DeleteMediaFileReason.NoLinkedEpisodes), Times.Exactly(10));
         }
 
         [Test]

src/NzbDrone.Core.Test/TvTests/EpisodeProviderTests/HandleEpisodeFileDeletedFixture.cs
@@ -58,7 +58,7 @@ namespace NzbDrone.Core.Test.TvTests.EpisodeProviderTests
         {
             GivenSingleEpisodeFile();
 
-            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, false));
+            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, DeleteMediaFileReason.MissingFromDisk));
 
             Mocker.GetMock<IEpisodeRepository>()
                 .Verify(v => v.Update(It.Is<Episode>(e => e.EpisodeFileId == 0)), Times.Once());
@@ -69,7 +69,7 @@ namespace NzbDrone.Core.Test.TvTests.EpisodeProviderTests
         {
             GivenMultiEpisodeFile();
 
-            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, false));
+            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, DeleteMediaFileReason.MissingFromDisk));
 
             Mocker.GetMock<IEpisodeRepository>()
                 .Verify(v => v.Update(It.Is<Episode>(e => e.EpisodeFileId == 0)), Times.Exactly(2));
@@ -84,7 +84,7 @@ namespace NzbDrone.Core.Test.TvTests.EpisodeProviderTests
                   .SetupGet(s => s.AutoUnmonitorPreviouslyDownloadedEpisodes)
                   .Returns(true);
 
-            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, false));
+            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, DeleteMediaFileReason.MissingFromDisk));
 
             Mocker.GetMock<IEpisodeRepository>()
                 .Verify(v => v.Update(It.Is<Episode>(e => e.Monitored == false)), Times.Once());
@@ -99,7 +99,7 @@ namespace NzbDrone.Core.Test.TvTests.EpisodeProviderTests
                   .SetupGet(s => s.AutoUnmonitorPreviouslyDownloadedEpisodes)
                   .Returns(false);
 
-            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, false));
+            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, DeleteMediaFileReason.Upgrade));
 
             Mocker.GetMock<IEpisodeRepository>()
                 .Verify(v => v.Update(It.Is<Episode>(e => e.Monitored == true)), Times.Once());
@@ -114,7 +114,7 @@ namespace NzbDrone.Core.Test.TvTests.EpisodeProviderTests
                   .SetupGet(s => s.AutoUnmonitorPreviouslyDownloadedEpisodes)
                   .Returns(true);
 
-            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, true));
+            Subject.Handle(new EpisodeFileDeletedEvent(_episodeFile, DeleteMediaFileReason.Upgrade));
 
             Mocker.GetMock<IEpisodeRepository>()
                 .Verify(v => v.Update(It.Is<Episode>(e => e.Monitored == true)), Times.Once());

src/NzbDrone.Core/MediaFiles/MediaFileTableCleanupService.cs
@@ -47,14 +47,14 @@ namespace NzbDrone.Core.MediaFiles
                     if (!_diskProvider.FileExists(episodeFilePath))
                     {
                         _logger.Debug("File [{0}] no longer exists on disk, removing from db", episodeFilePath);
-                        _mediaFileService.Delete(episodeFile);
+                        _mediaFileService.Delete(episodeFile, DeleteMediaFileReason.MissingFromDisk);
                         continue;
                     }
 
                     if (!episodes.Any(e => e.EpisodeFileId == episodeFile.Id))
                     {
                         _logger.Debug("File [{0}] is not assigned to any episodes, removing from db", episodeFilePath);
-                        _mediaFileService.Delete(episodeFile);
+                        _mediaFileService.Delete(episodeFile, DeleteMediaFileReason.NoLinkedEpisodes);
                         continue;
                     }
 