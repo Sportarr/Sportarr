nibty/Sonarr

Added RateLimit service to globally manager short duration ratelimits.

src/NzbDrone.Common/TPL/RateLimitService.cs
@@ -1,0 +1,43 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using NLog;
+using NzbDrone.Common.Cache;
+
+namespace NzbDrone.Common.TPL
+{
+    public interface IRateLimitService
+    {
+        void WaitAndPulse(string key, TimeSpan interval);
+    }
+
+    public class RateLimitService : IRateLimitService
+    {
+        private readonly ConcurrentDictionary<string, DateTime> _rateLimitStore;
+        private readonly Logger _logger;
+
+        public RateLimitService(ICacheManager cacheManager, Logger logger)
+        {
+            _rateLimitStore = cacheManager.GetCache<ConcurrentDictionary<string, DateTime>>(GetType(), "rateLimitStore").Get("rateLimitStore", () => new ConcurrentDictionary<string, DateTime>());
+            _logger = logger;
+        }
+
+        public void WaitAndPulse(string key, TimeSpan interval)
+        {
+            var waitTill = _rateLimitStore.AddOrUpdate(key,
+                (s) => DateTime.UtcNow + interval,
+                (s,i) => new DateTime(Math.Max(DateTime.UtcNow.Ticks, i.Ticks), DateTimeKind.Utc) + interval);
+
+            waitTill -= interval;
+
+            var delay = waitTill - DateTime.UtcNow;
+
+            if (delay.TotalSeconds > 0.0)
+            {
+                _logger.Trace("Rate Limit triggered, delaying '{0}' for {1:0.000} sec", key, delay.TotalSeconds);
+                System.Threading.Thread.Sleep(delay);
+            }
+        }
+    }
+}

src/NzbDrone.Core.Test/Framework/CoreTest.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using NzbDrone.Common.Cache;
 using NzbDrone.Common.Cloud;
 using NzbDrone.Common.Http;
+using NzbDrone.Common.TPL;
 using NzbDrone.Test.Common;
 
 namespace NzbDrone.Core.Test.Framework
@@ -17,7 +18,7 @@ namespace NzbDrone.Core.Test.Framework
         protected void UseRealHttp()
         {
             Mocker.SetConstant<IHttpProvider>(new HttpProvider(TestLogger));
-            Mocker.SetConstant<IHttpClient>(new HttpClient(Mocker.Resolve<CacheManager>(), TestLogger));
+            Mocker.SetConstant<IHttpClient>(new HttpClient(Mocker.Resolve<CacheManager>(), Mocker.Resolve<IRateLimitService>(), TestLogger));
             Mocker.SetConstant<IDroneServicesRequestBuilder>(new DroneServicesHttpRequestBuilder());
         }
     }

Sportarr/Sportarr

src/NzbDrone.Common/TPL/RateLimitService.cs
@@ -1,0 +1,43 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using NLog;
+using NzbDrone.Common.Cache;
+
+namespace NzbDrone.Common.TPL
+{
+    public interface IRateLimitService
+    {
+        void WaitAndPulse(string key, TimeSpan interval);
+    }
+
+    public class RateLimitService : IRateLimitService
+    {
+        private readonly ConcurrentDictionary<string, DateTime> _rateLimitStore;
+        private readonly Logger _logger;
+
+        public RateLimitService(ICacheManager cacheManager, Logger logger)
+        {
+            _rateLimitStore = cacheManager.GetCache<ConcurrentDictionary<string, DateTime>>(GetType(), "rateLimitStore").Get("rateLimitStore", () => new ConcurrentDictionary<string, DateTime>());
+            _logger = logger;
+        }
+
+        public void WaitAndPulse(string key, TimeSpan interval)
+        {
+            var waitUntil = _rateLimitStore.AddOrUpdate(key,
+                (s) => DateTime.UtcNow + interval,
+                (s,i) => new DateTime(Math.Max(DateTime.UtcNow.Ticks, i.Ticks), DateTimeKind.Utc) + interval);
+
+            waitUntil -= interval;
+
+            var delay = waitUntil - DateTime.UtcNow;
+
+            if (delay.TotalSeconds > 0.0)
+            {
+                _logger.Trace("Rate Limit triggered, delaying '{0}' for {1:0.000} sec", key, delay.TotalSeconds);
+                System.Threading.Thread.Sleep(delay);
+            }
+        }
+    }
+}

src/NzbDrone.Core.Test/Framework/CoreTest.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using NzbDrone.Common.Cache;
 using NzbDrone.Common.Cloud;
 using NzbDrone.Common.Http;
+using NzbDrone.Common.TPL;
 using NzbDrone.Test.Common;
 
 namespace NzbDrone.Core.Test.Framework
@@ -17,7 +18,7 @@ namespace NzbDrone.Core.Test.Framework
         protected void UseRealHttp()
         {
             Mocker.SetConstant<IHttpProvider>(new HttpProvider(TestLogger));
-            Mocker.SetConstant<IHttpClient>(new HttpClient(Mocker.Resolve<CacheManager>(), TestLogger));
+            Mocker.SetConstant<IHttpClient>(new HttpClient(Mocker.Resolve<CacheManager>(), Mocker.Resolve<RateLimitService>(), TestLogger));
             Mocker.SetConstant<IDroneServicesRequestBuilder>(new DroneServicesHttpRequestBuilder());
         }
     }