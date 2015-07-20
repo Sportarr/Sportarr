Terebi42/Sonarr

New: Will now temporarily stop using an indexer if the indexer reported an error.

src/NzbDrone.Core.Test/IndexerTests/TorrentRssIndexerTests/TestTorrentRssIndexer.cs
@@ -13,8 +13,8 @@ namespace NzbDrone.Core.Test.IndexerTests.TorrentRssIndexerTests
 {
     public class TestTorrentRssIndexer : TorrentRssIndexer
     {
-        public TestTorrentRssIndexer(IHttpClient httpClient, IConfigService configService, IParsingService parsingService, ITorrentRssParserFactory torrentRssParserFactory, Logger logger)
-            : base(httpClient, configService, parsingService, torrentRssParserFactory, logger)
+        public TestTorrentRssIndexer(ITorrentRssParserFactory torrentRssParserFactory, IIndexerStatusService indexerStatusService, IHttpClient httpClient, IConfigService configService, IParsingService parsingService, Logger logger)
+            : base(torrentRssParserFactory, indexerStatusService, httpClient, configService, parsingService, logger)
         {
         }
 

src/NzbDrone.Core/Indexers/Newznab/Newznab.cs
@@ -50,8 +50,8 @@ namespace NzbDrone.Core.Indexers.Newznab
             }
         }
 
-        public Newznab(IHttpClient httpClient, IConfigService configService, IParsingService parsingService, Logger logger)
-            : base(httpClient, configService, parsingService, logger)
+        public Newznab(IIndexerStatusService indexerStatusService, IHttpClient httpClient, IConfigService configService, IParsingService parsingService, Logger logger)
+            : base(indexerStatusService, httpClient, configService, parsingService, logger)
         {
 
         }

src/NzbDrone.Core/Indexers/Torznab/Torznab.cs
@@ -45,8 +45,8 @@ namespace NzbDrone.Core.Indexers.Torznab
             }
         }
 
-        public Torznab(IHttpClient httpClient, IConfigService configService, IParsingService parsingService, Logger logger)
-            : base(httpClient, configService, parsingService, logger)
+        public Torznab(IIndexerStatusService indexerStatusService, IHttpClient httpClient, IConfigService configService, IParsingService parsingService, Logger logger)
+            : base(indexerStatusService, httpClient, configService, parsingService, logger)
         {
 
         }

src/NzbDrone.Core/Indexers/Wombles/Wombles.cs
@@ -29,8 +29,8 @@ namespace NzbDrone.Core.Indexers.Wombles
             return new RssIndexerRequestGenerator("http://newshost.co.za/rss/?sec=TV&fr=false");
         }
 
-        public Wombles(IHttpClient httpClient, IConfigService configService, IParsingService parsingService, Logger logger)
-            : base(httpClient, configService, parsingService, logger)
+        public Wombles(IIndexerStatusService indexerStatusService, IHttpClient httpClient, IConfigService configService, IParsingService parsingService, Logger logger)
+            : base(indexerStatusService, httpClient, configService, parsingService, logger)
         {
 
         }

Sportarr/Sportarr

src/NzbDrone.Core.Test/IndexerTests/TorrentRssIndexerTests/TestTorrentRssIndexer.cs
@@ -13,8 +13,8 @@ namespace NzbDrone.Core.Test.IndexerTests.TorrentRssIndexerTests
 {
     public class TestTorrentRssIndexer : TorrentRssIndexer
     {
-        public TestTorrentRssIndexer(IHttpClient httpClient, IConfigService configService, IParsingService parsingService, ITorrentRssParserFactory torrentRssParserFactory, Logger logger)
-            : base(httpClient, configService, parsingService, torrentRssParserFactory, logger)
+        public TestTorrentRssIndexer(ITorrentRssParserFactory torrentRssParserFactory, IHttpClient httpClient, IIndexerStatusService indexerStatusService, IConfigService configService, IParsingService parsingService, Logger logger)
+            : base(torrentRssParserFactory, httpClient, indexerStatusService, configService, parsingService, logger)
         {
         }
 

src/NzbDrone.Core/Indexers/Newznab/Newznab.cs
@@ -50,8 +50,8 @@ namespace NzbDrone.Core.Indexers.Newznab
             }
         }
 
-        public Newznab(IHttpClient httpClient, IConfigService configService, IParsingService parsingService, Logger logger)
-            : base(httpClient, configService, parsingService, logger)
+        public Newznab(IHttpClient httpClient, IIndexerStatusService indexerStatusService, IConfigService configService, IParsingService parsingService, Logger logger)
+            : base(httpClient, indexerStatusService, configService, parsingService, logger)
         {
 
         }

src/NzbDrone.Core/Indexers/Torznab/Torznab.cs
@@ -48,8 +48,8 @@ namespace NzbDrone.Core.Indexers.Torznab
             }
         }
 
-        public Torznab(ITorznabCapabilitiesProvider torznabCapabilitiesProvider, IHttpClient httpClient, IConfigService configService, IParsingService parsingService, Logger logger)
-            : base(httpClient, configService, parsingService, logger)
+        public Torznab(ITorznabCapabilitiesProvider torznabCapabilitiesProvider, IHttpClient httpClient, IIndexerStatusService indexerStatusService, IConfigService configService, IParsingService parsingService, Logger logger)
+            : base(httpClient, indexerStatusService, configService, parsingService, logger)
         {
             _torznabCapabilitiesProvider = torznabCapabilitiesProvider;
         }

src/NzbDrone.Core/Indexers/Wombles/Wombles.cs
@@ -29,8 +29,8 @@ namespace NzbDrone.Core.Indexers.Wombles
             return new RssIndexerRequestGenerator("http://newshost.co.za/rss/?sec=TV&fr=false");
         }
 
-        public Wombles(IHttpClient httpClient, IConfigService configService, IParsingService parsingService, Logger logger)
-            : base(httpClient, configService, parsingService, logger)
+        public Wombles(IHttpClient httpClient, IIndexerStatusService indexerStatusService, IConfigService configService, IParsingService parsingService, Logger logger)
+            : base(httpClient, indexerStatusService, configService, parsingService, logger)
         {
 
         }