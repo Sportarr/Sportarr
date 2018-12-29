BoKKeR/Sonarr

Preferred words
New: Ability to prefer releases based on terms in release title

src/NzbDrone.Core/DecisionEngine/Specifications/RssSync/HistorySpecification.cs
@@ -5,6 +5,7 @@ using NzbDrone.Core.Configuration;
 using NzbDrone.Core.History;
 using NzbDrone.Core.IndexerSearch.Definitions;
 using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.Profiles.Releases;
 
 namespace NzbDrone.Core.DecisionEngine.Specifications.RssSync
 {
@@ -13,16 +14,19 @@ namespace NzbDrone.Core.DecisionEngine.Specifications.RssSync
         private readonly IHistoryService _historyService;
         private readonly UpgradableSpecification _upgradableSpecification;
         private readonly IConfigService _configService;
+        private readonly IPreferredWordService _preferredWordServiceCalculator;
         private readonly Logger _logger;
 
         public HistorySpecification(IHistoryService historyService,
                                            UpgradableSpecification upgradableSpecification,
                                            IConfigService configService,
+                                           IPreferredWordService preferredWordServiceCalculator,
                                            Logger logger)
         {
             _historyService = historyService;
             _upgradableSpecification = upgradableSpecification;
             _configService = configService;
+            _preferredWordServiceCalculator = preferredWordServiceCalculator;
             _logger = logger;
         }
 
@@ -48,8 +52,26 @@ namespace NzbDrone.Core.DecisionEngine.Specifications.RssSync
                 if (mostRecent != null && mostRecent.EventType == HistoryEventType.Grabbed)
                 {
                     var recent = mostRecent.Date.After(DateTime.UtcNow.AddHours(-12));
-                    var cutoffUnmet = _upgradableSpecification.CutoffNotMet(subject.Series.Profile, subject.Series.LanguageProfile, mostRecent.Quality, mostRecent.Language, subject.ParsedEpisodeInfo.Quality);
-                    var upgradeable = _upgradableSpecification.IsUpgradable(subject.Series.Profile, subject.Series.LanguageProfile, mostRecent.Quality, mostRecent.Language, subject.ParsedEpisodeInfo.Quality, subject.ParsedEpisodeInfo.Language);
+                    var preferredWordScore = _preferredWordServiceCalculator.Calculate(mostRecent.Series, mostRecent.SourceTitle);
+
+                    var cutoffUnmet = _upgradableSpecification.CutoffNotMet(
+                        subject.Series.Profile,
+                        subject.Series.LanguageProfile,
+                        mostRecent.Quality,
+                        mostRecent.Language,
+                        preferredWordScore,
+                        subject.ParsedEpisodeInfo.Quality,
+                        subject.PreferredWordScore);
+
+                    var upgradeable = _upgradableSpecification.IsUpgradable(
+                        subject.Series.Profile,
+                        subject.Series.LanguageProfile,
+                        mostRecent.Quality,
+                        mostRecent.Language,
+                        preferredWordScore,
+                        subject.ParsedEpisodeInfo.Quality,
+                        subject.ParsedEpisodeInfo.Language,
+                        subject.PreferredWordScore);
 
                     if (!recent && cdhEnabled)
                     {

src/Sonarr.Api.V3/Profiles/Release/ReleaseProfileModule.cs
@@ -1,0 +1,60 @@
+using System.Collections.Generic;
+using FluentValidation.Results;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Profiles.Releases;
+using Sonarr.Http;
+
+namespace Sonarr.Api.V3.Profiles.Release
+{
+    public class ReleaseProfileModule : SonarrRestModule<ReleaseProfileResource>
+    {
+        private readonly IReleaseProfileService _releaseProfileService;
+
+
+        public ReleaseProfileModule(IReleaseProfileService releaseProfileService)
+        {
+            _releaseProfileService = releaseProfileService;
+
+            GetResourceById = Get;
+            GetResourceAll = GetAll;
+            CreateResource = Create;
+            UpdateResource = Update;
+            DeleteResource = Delete;
+
+            SharedValidator.Custom(restriction =>
+            {
+                if (restriction.Ignored.IsNullOrWhiteSpace() && restriction.Required.IsNullOrWhiteSpace() && restriction.Preferred.Empty())
+                {
+                    return new ValidationFailure("", "'Must contain', 'Must not contain' or 'Peferrred' is required");
+                }
+
+                return null;
+            });
+        }
+
+        private ReleaseProfileResource Get(int id)
+        {
+            return _releaseProfileService.Get(id).ToResource();
+        }
+
+        private List<ReleaseProfileResource> GetAll()
+        {
+            return _releaseProfileService.All().ToResource();
+        }
+
+        private int Create(ReleaseProfileResource resource)
+        {
+            return _releaseProfileService.Add(resource.ToModel()).Id;
+        }
+
+        private void Update(ReleaseProfileResource resource)
+        {
+            _releaseProfileService.Update(resource.ToModel());
+        }
+
+        private void Delete(int id)
+        {
+            _releaseProfileService.Delete(id);
+        }
+    }
+}

Sportarr/Sportarr

src/NzbDrone.Core/DecisionEngine/Specifications/RssSync/HistorySpecification.cs
@@ -5,6 +5,7 @@ using NzbDrone.Core.Configuration;
 using NzbDrone.Core.History;
 using NzbDrone.Core.IndexerSearch.Definitions;
 using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.Profiles.Releases;
 
 namespace NzbDrone.Core.DecisionEngine.Specifications.RssSync
 {
@@ -13,16 +14,19 @@ namespace NzbDrone.Core.DecisionEngine.Specifications.RssSync
         private readonly IHistoryService _historyService;
         private readonly UpgradableSpecification _upgradableSpecification;
         private readonly IConfigService _configService;
+        private readonly IPreferredWordService _preferredWordServiceCalculator;
         private readonly Logger _logger;
 
         public HistorySpecification(IHistoryService historyService,
                                            UpgradableSpecification upgradableSpecification,
                                            IConfigService configService,
+                                           IPreferredWordService preferredWordServiceCalculator,
                                            Logger logger)
         {
             _historyService = historyService;
             _upgradableSpecification = upgradableSpecification;
             _configService = configService;
+            _preferredWordServiceCalculator = preferredWordServiceCalculator;
             _logger = logger;
         }
 
@@ -48,8 +52,29 @@ namespace NzbDrone.Core.DecisionEngine.Specifications.RssSync
                 if (mostRecent != null && mostRecent.EventType == HistoryEventType.Grabbed)
                 {
                     var recent = mostRecent.Date.After(DateTime.UtcNow.AddHours(-12));
-                    var cutoffUnmet = _upgradableSpecification.CutoffNotMet(subject.Series.Profile, subject.Series.LanguageProfile, mostRecent.Quality, mostRecent.Language, subject.ParsedEpisodeInfo.Quality);
-                    var upgradeable = _upgradableSpecification.IsUpgradable(subject.Series.Profile, subject.Series.LanguageProfile, mostRecent.Quality, mostRecent.Language, subject.ParsedEpisodeInfo.Quality, subject.ParsedEpisodeInfo.Language);
+
+                    // The series will be the same as the one in history since it's the same episode.
+                    // Instead of fetching the series from the DB reuse the known series.
+                    var preferredWordScore = _preferredWordServiceCalculator.Calculate(subject.Series, mostRecent.SourceTitle);
+
+                    var cutoffUnmet = _upgradableSpecification.CutoffNotMet(
+                        subject.Series.Profile,
+                        subject.Series.LanguageProfile,
+                        mostRecent.Quality,
+                        mostRecent.Language,
+                        preferredWordScore,
+                        subject.ParsedEpisodeInfo.Quality,
+                        subject.PreferredWordScore);
+
+                    var upgradeable = _upgradableSpecification.IsUpgradable(
+                        subject.Series.Profile,
+                        subject.Series.LanguageProfile,
+                        mostRecent.Quality,
+                        mostRecent.Language,
+                        preferredWordScore,
+                        subject.ParsedEpisodeInfo.Quality,
+                        subject.ParsedEpisodeInfo.Language,
+                        subject.PreferredWordScore);
 
                     if (!recent && cdhEnabled)
                     {

src/Sonarr.Api.V3/Profiles/Release/ReleaseProfileModule.cs
@@ -1,0 +1,60 @@
+using System.Collections.Generic;
+using FluentValidation.Results;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Profiles.Releases;
+using Sonarr.Http;
+
+namespace Sonarr.Api.V3.Profiles.Release
+{
+    public class ReleaseProfileModule : SonarrRestModule<ReleaseProfileResource>
+    {
+        private readonly IReleaseProfileService _releaseProfileService;
+
+
+        public ReleaseProfileModule(IReleaseProfileService releaseProfileService)
+        {
+            _releaseProfileService = releaseProfileService;
+
+            GetResourceById = Get;
+            GetResourceAll = GetAll;
+            CreateResource = Create;
+            UpdateResource = Update;
+            DeleteResource = Delete;
+
+            SharedValidator.Custom(restriction =>
+            {
+                if (restriction.Ignored.IsNullOrWhiteSpace() && restriction.Required.IsNullOrWhiteSpace() && restriction.Preferred.Empty())
+                {
+                    return new ValidationFailure("", "'Must contain', 'Must not contain' or 'Preferred' is required");
+                }
+
+                return null;
+            });
+        }
+
+        private ReleaseProfileResource Get(int id)
+        {
+            return _releaseProfileService.Get(id).ToResource();
+        }
+
+        private List<ReleaseProfileResource> GetAll()
+        {
+            return _releaseProfileService.All().ToResource();
+        }
+
+        private int Create(ReleaseProfileResource resource)
+        {
+            return _releaseProfileService.Add(resource.ToModel()).Id;
+        }
+
+        private void Update(ReleaseProfileResource resource)
+        {
+            _releaseProfileService.Update(resource.ToModel());
+        }
+
+        private void Delete(int id)
+        {
+            _releaseProfileService.Delete(id);
+        }
+    }
+}