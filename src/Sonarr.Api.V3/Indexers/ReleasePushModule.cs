Ablecken/Sonarr

Release module validation in v3

src/Sonarr.Api.V3/Indexers/ReleasePushModule.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using FluentValidation;
+using FluentValidation.Results;
 using Nancy;
 using Nancy.ModelBinding;
 using NLog;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Datastore;
 using NzbDrone.Core.DecisionEngine;
 using NzbDrone.Core.Download;
+using NzbDrone.Core.Indexers;
 using NzbDrone.Core.Parser.Model;
 using Sonarr.Http.Extensions;
+using Sonarr.Http.REST;
 
 namespace Sonarr.Api.V3.Indexers
 {
@@ -15,36 +20,92 @@ namespace Sonarr.Api.V3.Indexers
     {
         private readonly IMakeDownloadDecision _downloadDecisionMaker;
         private readonly IProcessDownloadDecisions _downloadDecisionProcessor;
+        private readonly IIndexerFactory _indexerFactory;
         private readonly Logger _logger;
+        private ResourceValidator<ReleaseResource> _releaseValidator;
 
         public ReleasePushModule(IMakeDownloadDecision downloadDecisionMaker,
                                  IProcessDownloadDecisions downloadDecisionProcessor,
+                                 IIndexerFactory indexerFactory,
                                  Logger logger)
         {
             _downloadDecisionMaker = downloadDecisionMaker;
             _downloadDecisionProcessor = downloadDecisionProcessor;
+            _indexerFactory = indexerFactory;
             _logger = logger;
+;
+            _releaseValidator = new ResourceValidator<ReleaseResource>();
+            _releaseValidator.RuleFor(s => s.Title).NotEmpty();
+            _releaseValidator.RuleFor(s => s.DownloadUrl).NotEmpty();
+            _releaseValidator.RuleFor(s => s.DownloadProtocol).NotEmpty();
+            _releaseValidator.RuleFor(s => s.PublishDate).NotEmpty();
 
             Post["/push"] = x => ProcessRelease(this.Bind<ReleaseResource>());
-
-            PostValidator.RuleFor(s => s.Title).NotEmpty();
-            PostValidator.RuleFor(s => s.DownloadUrl).NotEmpty();
-            PostValidator.RuleFor(s => s.DownloadProtocol).NotEmpty();
-            PostValidator.RuleFor(s => s.PublishDate).NotEmpty();
         }
 
         private Response ProcessRelease(ReleaseResource release)
         {
+            var validationFailures = _releaseValidator.Validate(release).Errors;
+
+            if (validationFailures.Any())
+            {
+                throw new ValidationException(validationFailures);
+            }
+
             _logger.Info("Release pushed: {0} - {1}", release.Title, release.DownloadUrl);
 
             var info = release.ToModel();
 
             info.Guid = "PUSH-" + info.DownloadUrl;
 
+            ResolveIndexer(info);
+
             var decisions = _downloadDecisionMaker.GetRssDecision(new List<ReleaseInfo> { info });
             _downloadDecisionProcessor.ProcessDecisions(decisions);
 
-            return MapDecisions(decisions).First().AsResponse();
+            var firstDecision = decisions.FirstOrDefault();
+
+            if (firstDecision?.RemoteEpisode.ParsedEpisodeInfo == null)
+            {
+                throw new ValidationException(new List<ValidationFailure>{ new ValidationFailure("Title", "Unable to parse", release.Title) });
+            }
+
+            return MapDecisions(new [] { firstDecision }).AsResponse();
+        }
+
+        private void ResolveIndexer(ReleaseInfo release)
+        {
+            if (release.IndexerId == 0 && release.Indexer.IsNotNullOrWhiteSpace())
+            {
+                var indexer = _indexerFactory.All().FirstOrDefault(v => v.Name == release.Indexer);
+                if (indexer != null)
+                {
+                    release.IndexerId = indexer.Id;
+                    _logger.Debug("Push Release {0} associated with indexer {1} - {2}.", release.Title, release.IndexerId, release.Indexer);
+                }
+                else
+                {
+                    _logger.Debug("Push Release {0} not associated with unknown indexer {1}.", release.Title, release.Indexer);
+                }
+            }
+            else if (release.IndexerId != 0 && release.Indexer.IsNullOrWhiteSpace())
+            {
+                try
+                {
+                    var indexer = _indexerFactory.Get(release.IndexerId);
+                    release.Indexer = indexer.Name;
+                    _logger.Debug("Push Release {0} associated with indexer {1} - {2}.", release.Title, release.IndexerId, release.Indexer);
+                }
+                catch (ModelNotFoundException)
+                {
+                    _logger.Debug("Push Release {0} not associated with unknown indexer {0}.", release.Title, release.IndexerId);
+                    release.IndexerId = 0;
+                }
+            }
+            else
+            {
+                _logger.Debug("Push Release {0} not associated with an indexer.", release.Title);
+            }
         }
     }
 }

Sportarr/Sportarr

src/Sonarr.Api.V3/Indexers/ReleasePushModule.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using FluentValidation;
+using FluentValidation.Results;
 using Nancy;
 using Nancy.ModelBinding;
 using NLog;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Datastore;
 using NzbDrone.Core.DecisionEngine;
 using NzbDrone.Core.Download;
+using NzbDrone.Core.Indexers;
 using NzbDrone.Core.Parser.Model;
 using Sonarr.Http.Extensions;
+using Sonarr.Http.REST;
 
 namespace Sonarr.Api.V3.Indexers
 {
@@ -15,36 +20,92 @@ namespace Sonarr.Api.V3.Indexers
     {
         private readonly IMakeDownloadDecision _downloadDecisionMaker;
         private readonly IProcessDownloadDecisions _downloadDecisionProcessor;
+        private readonly IIndexerFactory _indexerFactory;
+        private ResourceValidator<ReleaseResource> _releaseValidator;
         private readonly Logger _logger;
 
         public ReleasePushModule(IMakeDownloadDecision downloadDecisionMaker,
                                  IProcessDownloadDecisions downloadDecisionProcessor,
+                                 IIndexerFactory indexerFactory,
                                  Logger logger)
         {
             _downloadDecisionMaker = downloadDecisionMaker;
             _downloadDecisionProcessor = downloadDecisionProcessor;
+            _indexerFactory = indexerFactory;
             _logger = logger;
 
-            Post["/push"] = x => ProcessRelease(this.Bind<ReleaseResource>());
+            _releaseValidator = new ResourceValidator<ReleaseResource>();
+            _releaseValidator.RuleFor(s => s.Title).NotEmpty();
+            _releaseValidator.RuleFor(s => s.DownloadUrl).NotEmpty();
+            _releaseValidator.RuleFor(s => s.DownloadProtocol).NotEmpty();
+            _releaseValidator.RuleFor(s => s.PublishDate).NotEmpty();
 
-            PostValidator.RuleFor(s => s.Title).NotEmpty();
-            PostValidator.RuleFor(s => s.DownloadUrl).NotEmpty();
-            PostValidator.RuleFor(s => s.DownloadProtocol).NotEmpty();
-            PostValidator.RuleFor(s => s.PublishDate).NotEmpty();
+            Post["/push"] = x => ProcessRelease(this.Bind<ReleaseResource>());
         }
 
         private Response ProcessRelease(ReleaseResource release)
         {
+            var validationFailures = _releaseValidator.Validate(release).Errors;
+
+            if (validationFailures.Any())
+            {
+                throw new ValidationException(validationFailures);
+            }
+
             _logger.Info("Release pushed: {0} - {1}", release.Title, release.DownloadUrl);
 
             var info = release.ToModel();
 
             info.Guid = "PUSH-" + info.DownloadUrl;
 
+            ResolveIndexer(info);
+
             var decisions = _downloadDecisionMaker.GetRssDecision(new List<ReleaseInfo> { info });
             _downloadDecisionProcessor.ProcessDecisions(decisions);
 
-            return MapDecisions(decisions).First().AsResponse();
+            var firstDecision = decisions.FirstOrDefault();
+
+            if (firstDecision?.RemoteEpisode.ParsedEpisodeInfo == null)
+            {
+                throw new ValidationException(new List<ValidationFailure>{ new ValidationFailure("Title", "Unable to parse", release.Title) });
+            }
+
+            return MapDecisions(new [] { firstDecision }).AsResponse();
+        }
+
+        private void ResolveIndexer(ReleaseInfo release)
+        {
+            if (release.IndexerId == 0 && release.Indexer.IsNotNullOrWhiteSpace())
+            {
+                var indexer = _indexerFactory.All().FirstOrDefault(v => v.Name == release.Indexer);
+                if (indexer != null)
+                {
+                    release.IndexerId = indexer.Id;
+                    _logger.Debug("Push Release {0} associated with indexer {1} - {2}.", release.Title, release.IndexerId, release.Indexer);
+                }
+                else
+                {
+                    _logger.Debug("Push Release {0} not associated with known indexer {1}.", release.Title, release.Indexer);
+                }
+            }
+            else if (release.IndexerId != 0 && release.Indexer.IsNullOrWhiteSpace())
+            {
+                try
+                {
+                    var indexer = _indexerFactory.Get(release.IndexerId);
+                    release.Indexer = indexer.Name;
+                    _logger.Debug("Push Release {0} associated with indexer {1} - {2}.", release.Title, release.IndexerId, release.Indexer);
+                }
+                catch (ModelNotFoundException)
+                {
+                    _logger.Debug("Push Release {0} not associated with known indexer {0}.", release.Title, release.IndexerId);
+                    release.IndexerId = 0;
+                }
+            }
+            else
+            {
+                _logger.Debug("Push Release {0} not associated with an indexer.", release.Title);
+            }
         }
     }
 }