Radarr/Radarr

Fixed: False positive in remote path check with transmission
Correctly use the download directory when it's set

src/NzbDrone.Core/Download/Clients/Transmission/TransmissionBase.cs
@@ -166,12 +166,20 @@ namespace NzbDrone.Core.Download.Clients.Transmission
 
         public override DownloadClientInfo GetStatus()
         {
-            var config = _proxy.GetConfig(Settings);
-            var destDir = config.DownloadDir;
-
-            if (Settings.MovieCategory.IsNotNullOrWhiteSpace())
+            string destDir;
+            if (Settings.MovieDirectory.IsNotNullOrWhiteSpace())
             {
-                destDir = string.Format("{0}/.{1}", destDir, Settings.MovieCategory);
+                destDir = Settings.MovieDirectory;
+            }
+            else
+            {
+                var config = _proxy.GetConfig(Settings);
+                destDir = config.DownloadDir;
+
+                if (Settings.MovieCategory.IsNotNullOrWhiteSpace())
+                {
+                    destDir = string.Format("{0}/{1}", destDir, Settings.MovieCategory);
+                }
             }
 
             return new DownloadClientInfo

Sportarr/Sportarr

(cherry picked from commit e09ca145d125a12016a6bf41a4971148bd2de870)

src/NzbDrone.Core/Download/Clients/Transmission/TransmissionBase.cs
@@ -171,12 +171,21 @@ namespace NzbDrone.Core.Download.Clients.Transmission
 
         public override DownloadClientInfo GetStatus()
         {
-            var config = _proxy.GetConfig(Settings);
-            var destDir = config.DownloadDir;
+            string destDir;
 
-            if (Settings.TvCategory.IsNotNullOrWhiteSpace())
+            if (Settings.TvDirectory.IsNotNullOrWhiteSpace())
             {
-                destDir = string.Format("{0}/{1}", destDir, Settings.TvCategory);
+                destDir = Settings.TvDirectory;
+            }
+            else
+            {
+                var config = _proxy.GetConfig(Settings);
+                destDir = config.DownloadDir;
+
+                if (Settings.TvCategory.IsNotNullOrWhiteSpace())
+                {
+                    destDir = $"{destDir}/{Settings.TvCategory}";
+                }
             }
 
             return new DownloadClientInfo