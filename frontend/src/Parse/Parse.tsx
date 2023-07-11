nicksen/Sonarr

New: Added UI for parsing release names
Closes #5263

frontend/src/System/Parse/Parse.tsx
@@ -1,0 +1,111 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import TextInput from 'Components/Form/TextInput';
+import Icon from 'Components/Icon';
+import Button from 'Components/Link/Button';
+import LoadingIndicator from 'Components/Loading/LoadingIndicator';
+import PageContent from 'Components/Page/PageContent';
+import PageContentBody from 'Components/Page/PageContentBody';
+import { icons } from 'Helpers/Props';
+import { clear, fetch } from 'Store/Actions/parseActions';
+import getErrorMessage from 'Utilities/Object/getErrorMessage';
+import ParseResult from './ParseResult';
+import parseStateSelector from './parseStateSelector';
+import styles from './Parse.css';
+
+function Parse() {
+  const { isFetching, error, item } = useSelector(parseStateSelector());
+
+  const [title, setTitle] = useState('');
+  const dispatch = useDispatch();
+
+  const onInputChange = useCallback(
+    ({ value }: { value: string }) => {
+      const trimmedValue = value.trim();
+
+      setTitle(trimmedValue);
+
+      if (trimmedValue === '') {
+        dispatch(clear());
+      } else {
+        dispatch(fetch({ title: trimmedValue }));
+      }
+    },
+    [setTitle, dispatch]
+  );
+
+  const onClearPress = useCallback(() => {
+    setTitle('');
+    dispatch(fetch({ title: '' }));
+  }, [setTitle, dispatch]);
+
+  useEffect(
+    () => {
+      return () => {
+        dispatch(clear());
+      };
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
+  return (
+    <PageContent title="Parse">
+      <PageContentBody>
+        <div className={styles.inputContainer}>
+          <div className={styles.inputIconContainer}>
+            <Icon name={icons.PARSE} size={20} />
+          </div>
+
+          <TextInput
+            className={styles.input}
+            name="title"
+            value={title}
+            placeholder="eg. Series.Title.S01E05.720p.HDTV-RlsGroup"
+            autoFocus={true}
+            onChange={onInputChange}
+          />
+
+          <Button className={styles.clearButton} onPress={onClearPress}>
+            <Icon name={icons.REMOVE} size={20} />
+          </Button>
+        </div>
+
+        {isFetching ? <LoadingIndicator /> : null}
+
+        {!isFetching && !!error ? (
+          <div className={styles.message}>
+            <div className={styles.helpText}>
+              Error parsing, please try again.
+            </div>
+            <div>{getErrorMessage(error)}</div>
+          </div>
+        ) : null}
+
+        {!isFetching && title && !error && !item.parsedEpisodeInfo ? (
+          <div className={styles.message}>
+            Unable to parse the provided title, please try again.
+          </div>
+        ) : null}
+
+        {!isFetching && !error && item.parsedEpisodeInfo ? (
+          <ParseResult />
+        ) : null}
+
+        {title ? null : (
+          <div className={styles.message}>
+            <div className={styles.helpText}>
+              Enter a release title in the input above
+            </div>
+            <div>
+              Sonarr will attempt to parse the title and show you details about
+              it
+            </div>
+          </div>
+        )}
+      </PageContentBody>
+    </PageContent>
+  );
+}
+
+export default Parse;

frontend/src/System/Parse/ParseResult.tsx
@@ -1,0 +1,219 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import FieldSet from 'Components/FieldSet';
+import EpisodeFormats from 'Episode/EpisodeFormats';
+import SeriesTitleLink from 'Series/SeriesTitleLink';
+import translate from 'Utilities/String/translate';
+import ParseResultItem from './ParseResultItem';
+import parseStateSelector from './parseStateSelector';
+
+function ParseResult() {
+  const { item } = useSelector(parseStateSelector());
+  const {
+    customFormats,
+    customFormatScore,
+    episodes,
+    languages,
+    parsedEpisodeInfo,
+    series,
+  } = item;
+  const {
+    releaseTitle,
+    seriesTitle,
+    seriesTitleInfo,
+    releaseGroup,
+    releaseHash,
+    seasonNumber,
+    episodeNumbers,
+    absoluteEpisodeNumbers,
+    special,
+    fullSeason,
+    isMultiSeason,
+    isPartialSeason,
+    isDaily,
+    airDate,
+    quality,
+  } = parsedEpisodeInfo;
+
+  return (
+    <div>
+      <FieldSet legend={translate('Release')}>
+        <ParseResultItem
+          title={translate('Release Title')}
+          data={releaseTitle}
+        />
+
+        <ParseResultItem title={translate('Series Title')} data={seriesTitle} />
+
+        <ParseResultItem
+          title={translate('Year')}
+          data={seriesTitleInfo.year > 0 ? seriesTitleInfo.year : '-'}
+        />
+
+        <ParseResultItem
+          title={translate('All Titles')}
+          data={
+            seriesTitleInfo.allTitles?.length > 0
+              ? seriesTitleInfo.allTitles.join(', ')
+              : '-'
+          }
+        />
+
+        <ParseResultItem
+          title={translate('Release Group')}
+          data={releaseGroup ?? '-'}
+        />
+
+        <ParseResultItem
+          title={translate('Release Hash')}
+          data={releaseHash ? releaseHash : '-'}
+        />
+      </FieldSet>
+
+      {/* 
+      
+      Year
+      Secondary titles
+      special episode
+      
+      */}
+
+      <FieldSet legend={translate('Episode Info')}>
+        <ParseResultItem
+          title={translate('Season Number')}
+          data={
+            seasonNumber === 0 && absoluteEpisodeNumbers.length
+              ? '-'
+              : seasonNumber
+          }
+        />
+
+        <ParseResultItem
+          title={translate('Episode Number(s)')}
+          data={episodeNumbers.join(', ') || '-'}
+        />
+
+        <ParseResultItem
+          title={translate('Absolute Episode Number(s)')}
+          data={
+            absoluteEpisodeNumbers.length
+              ? absoluteEpisodeNumbers.join(', ')
+              : '-'
+          }
+        />
+
+        <ParseResultItem
+          title={translate('Special')}
+          data={special ? 'True' : 'False'}
+        />
+
+        <ParseResultItem
+          title={translate('Full Season')}
+          data={fullSeason ? 'True' : 'False'}
+        />
+
+        <ParseResultItem
+          title={translate('Multi-Season')}
+          data={isMultiSeason ? 'True' : 'False'}
+        />
+
+        <ParseResultItem
+          title={translate('Partial Season')}
+          data={isPartialSeason ? 'True' : 'False'}
+        />
+
+        <ParseResultItem
+          title={translate('Daily')}
+          data={isDaily ? 'True' : 'False'}
+        />
+
+        <ParseResultItem title={translate('Air Date')} data={airDate ?? '-'} />
+      </FieldSet>
+
+      <FieldSet legend={translate('Quality')}>
+        <ParseResultItem
+          title={translate('Quality')}
+          data={quality.quality.name}
+        />
+
+        <ParseResultItem
+          title={translate('Proper')}
+          data={quality.revision.version > 1 ? quality.revision.version : '-'}
+        />
+
+        <ParseResultItem
+          title={translate('Real')}
+          data={quality.revision.real ? quality.revision.real : '-'}
+        />
+
+        <ParseResultItem
+          title={translate('Repack')}
+          data={quality.revision.isRepack ? 'True' : '-'}
+        />
+      </FieldSet>
+
+      <FieldSet legend={translate('Language')}>
+        <ParseResultItem
+          title={translate('Language')}
+          data={languages.map((l) => l.name).join(', ')}
+        />
+      </FieldSet>
+
+      <FieldSet legend={translate('Details')}>
+        <ParseResultItem
+          title={translate('Matched to Series')}
+          data={
+            series ? (
+              <SeriesTitleLink
+                titleSlug={series.titleSlug}
+                title={series.title}
+              />
+            ) : (
+              '-'
+            )
+          }
+        />
+
+        <ParseResultItem
+          title={translate('Matched to Season')}
+          data={episodes.length ? episodes[0].seasonNumber : '-'}
+        />
+
+        <ParseResultItem
+          title={translate('Matched to Episodes')}
+          data={
+            episodes.length ? (
+              <div>
+                {episodes.map((e) => {
+                  return (
+                    <div key={e.id}>
+                      {e.episodeNumber}
+                      {series?.seriesType === 'anime' && e.absoluteEpisodeNumber
+                        ? ` (${e.absoluteEpisodeNumber})`
+                        : ''}{' '}
+                      {` - ${e.title}`}
+                    </div>
+                  );
+                })}
+              </div>
+            ) : (
+              '-'
+            )
+          }
+        />
+
+        <ParseResultItem
+          title={translate('Custom Formats')}
+          data={<EpisodeFormats formats={customFormats} />}
+        />
+
+        <ParseResultItem
+          title={translate('Custom Format Score')}
+          data={customFormatScore}
+        />
+      </FieldSet>
+    </div>
+  );
+}
+
+export default ParseResult;

Sportarr/Sportarr

frontend/src/Parse/Parse.tsx
@@ -1,0 +1,111 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import TextInput from 'Components/Form/TextInput';
+import Icon from 'Components/Icon';
+import Button from 'Components/Link/Button';
+import LoadingIndicator from 'Components/Loading/LoadingIndicator';
+import PageContent from 'Components/Page/PageContent';
+import PageContentBody from 'Components/Page/PageContentBody';
+import { icons } from 'Helpers/Props';
+import { clear, fetch } from 'Store/Actions/parseActions';
+import getErrorMessage from 'Utilities/Object/getErrorMessage';
+import ParseResult from './ParseResult';
+import parseStateSelector from './parseStateSelector';
+import styles from './Parse.css';
+
+function Parse() {
+  const { isFetching, error, item } = useSelector(parseStateSelector());
+
+  const [title, setTitle] = useState('');
+  const dispatch = useDispatch();
+
+  const onInputChange = useCallback(
+    ({ value }: { value: string }) => {
+      const trimmedValue = value.trim();
+
+      setTitle(value);
+
+      if (trimmedValue === '') {
+        dispatch(clear());
+      } else {
+        dispatch(fetch({ title: trimmedValue }));
+      }
+    },
+    [setTitle, dispatch]
+  );
+
+  const onClearPress = useCallback(() => {
+    setTitle('');
+    dispatch(fetch({ title: '' }));
+  }, [setTitle, dispatch]);
+
+  useEffect(
+    () => {
+      return () => {
+        dispatch(clear());
+      };
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
+  return (
+    <PageContent title="Parse">
+      <PageContentBody>
+        <div className={styles.inputContainer}>
+          <div className={styles.inputIconContainer}>
+            <Icon name={icons.PARSE} size={20} />
+          </div>
+
+          <TextInput
+            className={styles.input}
+            name="title"
+            value={title}
+            placeholder="eg. Series.Title.S01E05.720p.HDTV-RlsGroup"
+            autoFocus={true}
+            onChange={onInputChange}
+          />
+
+          <Button className={styles.clearButton} onPress={onClearPress}>
+            <Icon name={icons.REMOVE} size={20} />
+          </Button>
+        </div>
+
+        {isFetching ? <LoadingIndicator /> : null}
+
+        {!isFetching && !!error ? (
+          <div className={styles.message}>
+            <div className={styles.helpText}>
+              Error parsing, please try again.
+            </div>
+            <div>{getErrorMessage(error)}</div>
+          </div>
+        ) : null}
+
+        {!isFetching && title && !error && !item.parsedEpisodeInfo ? (
+          <div className={styles.message}>
+            Unable to parse the provided title, please try again.
+          </div>
+        ) : null}
+
+        {!isFetching && !error && item.parsedEpisodeInfo ? (
+          <ParseResult item={item} />
+        ) : null}
+
+        {title ? null : (
+          <div className={styles.message}>
+            <div className={styles.helpText}>
+              Enter a release title in the input above
+            </div>
+            <div>
+              Sonarr will attempt to parse the title and show you details about
+              it
+            </div>
+          </div>
+        )}
+      </PageContentBody>
+    </PageContent>
+  );
+}
+
+export default Parse;

frontend/src/Parse/ParseResult.tsx
@@ -1,0 +1,234 @@
+import React from 'react';
+import { ParseModel } from 'App/State/ParseAppState';
+import FieldSet from 'Components/FieldSet';
+import EpisodeFormats from 'Episode/EpisodeFormats';
+import SeriesTitleLink from 'Series/SeriesTitleLink';
+import translate from 'Utilities/String/translate';
+import ParseResultItem from './ParseResultItem';
+
+interface ParseResultProps {
+  item: ParseModel;
+}
+
+function ParseResult(props: ParseResultProps) {
+  const { item } = props;
+  const {
+    customFormats,
+    customFormatScore,
+    episodes,
+    languages,
+    parsedEpisodeInfo,
+    series,
+  } = item;
+
+  const {
+    releaseTitle,
+    seriesTitle,
+    seriesTitleInfo,
+    releaseGroup,
+    releaseHash,
+    seasonNumber,
+    episodeNumbers,
+    absoluteEpisodeNumbers,
+    special,
+    fullSeason,
+    isMultiSeason,
+    isPartialSeason,
+    isDaily,
+    airDate,
+    quality,
+  } = parsedEpisodeInfo;
+
+  const finalLanguages = languages ?? parsedEpisodeInfo.languages;
+
+  return (
+    <div>
+      <FieldSet legend={translate('Release')}>
+        <ParseResultItem
+          title={translate('Release Title')}
+          data={releaseTitle}
+        />
+
+        <ParseResultItem title={translate('Series Title')} data={seriesTitle} />
+
+        <ParseResultItem
+          title={translate('Year')}
+          data={seriesTitleInfo.year > 0 ? seriesTitleInfo.year : '-'}
+        />
+
+        <ParseResultItem
+          title={translate('All Titles')}
+          data={
+            seriesTitleInfo.allTitles?.length > 0
+              ? seriesTitleInfo.allTitles.join(', ')
+              : '-'
+          }
+        />
+
+        <ParseResultItem
+          title={translate('Release Group')}
+          data={releaseGroup ?? '-'}
+        />
+
+        <ParseResultItem
+          title={translate('Release Hash')}
+          data={releaseHash ? releaseHash : '-'}
+        />
+      </FieldSet>
+
+      {/* 
+      
+      Year
+      Secondary titles
+      special episode
+      
+      */}
+
+      <FieldSet legend={translate('Episode Info')}>
+        <ParseResultItem
+          title={translate('Season Number')}
+          data={
+            seasonNumber === 0 && absoluteEpisodeNumbers.length
+              ? '-'
+              : seasonNumber
+          }
+        />
+
+        <ParseResultItem
+          title={translate('Episode Number(s)')}
+          data={episodeNumbers.join(', ') || '-'}
+        />
+
+        <ParseResultItem
+          title={translate('Absolute Episode Number(s)')}
+          data={
+            absoluteEpisodeNumbers.length
+              ? absoluteEpisodeNumbers.join(', ')
+              : '-'
+          }
+        />
+
+        <ParseResultItem
+          title={translate('Special')}
+          data={special ? 'True' : 'False'}
+        />
+
+        <ParseResultItem
+          title={translate('Full Season')}
+          data={fullSeason ? 'True' : 'False'}
+        />
+
+        <ParseResultItem
+          title={translate('Multi-Season')}
+          data={isMultiSeason ? 'True' : 'False'}
+        />
+
+        <ParseResultItem
+          title={translate('Partial Season')}
+          data={isPartialSeason ? 'True' : 'False'}
+        />
+
+        <ParseResultItem
+          title={translate('Daily')}
+          data={isDaily ? 'True' : 'False'}
+        />
+
+        <ParseResultItem title={translate('Air Date')} data={airDate ?? '-'} />
+      </FieldSet>
+
+      <FieldSet legend={translate('Quality')}>
+        <ParseResultItem
+          title={translate('Quality')}
+          data={quality.quality.name}
+        />
+
+        <ParseResultItem
+          title={translate('Version')}
+          data={quality.revision.version > 1 ? quality.revision.version : '-'}
+        />
+
+        <ParseResultItem
+          title={translate('Real')}
+          data={quality.revision.real ? 'True' : '-'}
+        />
+
+        <ParseResultItem
+          title={translate('Proper')}
+          data={
+            quality.revision.version > 1 && !quality.revision.isRepack
+              ? 'True'
+              : '-'
+          }
+        />
+
+        <ParseResultItem
+          title={translate('Repack')}
+          data={quality.revision.isRepack ? 'True' : '-'}
+        />
+      </FieldSet>
+
+      <FieldSet legend={translate('Languages')}>
+        <ParseResultItem
+          title={translate('Languages')}
+          data={finalLanguages.map((l) => l.name).join(', ')}
+        />
+      </FieldSet>
+
+      <FieldSet legend={translate('Details')}>
+        <ParseResultItem
+          title={translate('Matched to Series')}
+          data={
+            series ? (
+              <SeriesTitleLink
+                titleSlug={series.titleSlug}
+                title={series.title}
+              />
+            ) : (
+              '-'
+            )
+          }
+        />
+
+        <ParseResultItem
+          title={translate('Matched to Season')}
+          data={episodes.length ? episodes[0].seasonNumber : '-'}
+        />
+
+        <ParseResultItem
+          title={translate('Matched to Episodes')}
+          data={
+            episodes.length ? (
+              <div>
+                {episodes.map((e) => {
+                  return (
+                    <div key={e.id}>
+                      {e.episodeNumber}
+                      {series?.seriesType === 'anime' && e.absoluteEpisodeNumber
+                        ? ` (${e.absoluteEpisodeNumber})`
+                        : ''}{' '}
+                      {` - ${e.title}`}
+                    </div>
+                  );
+                })}
+              </div>
+            ) : (
+              '-'
+            )
+          }
+        />
+
+        <ParseResultItem
+          title={translate('Custom Formats')}
+          data={<EpisodeFormats formats={customFormats} />}
+        />
+
+        <ParseResultItem
+          title={translate('Custom Format Score')}
+          data={customFormatScore}
+        />
+      </FieldSet>
+    </div>
+  );
+}
+
+export default ParseResult;