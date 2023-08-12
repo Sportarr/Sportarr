jamesmacwhite/Lidarr

New: Postgres Database Support
Co-Authored-By: Qstick <376117+Qstick@users.noreply.github.com>

src/NzbDrone.Core/Housekeeping/Housekeepers/CleanupOrphanedMetadataFiles.cs
@@ -25,12 +25,12 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
         {
             using (var mapper = _database.OpenConnection())
             {
-                mapper.Execute(@"DELETE FROM MetadataFiles
-                                     WHERE Id IN (
-                                     SELECT MetadataFiles.Id FROM MetadataFiles
-                                     LEFT OUTER JOIN Artists
-                                     ON MetadataFiles.ArtistId = Artists.Id
-                                     WHERE Artists.Id IS NULL)");
+                mapper.Execute(@"DELETE FROM ""MetadataFiles""
+                                     WHERE ""Id"" IN (
+                                     SELECT ""MetadataFiles"".""Id"" FROM ""MetadataFiles""
+                                     LEFT OUTER JOIN ""Artists""
+                                     ON ""MetadataFiles"".""ArtistId"" = ""Artists"".""Id""
+                                     WHERE ""Artists"".""Id"" IS NULL)");
             }
         }
 
@@ -38,13 +38,13 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
         {
             using (var mapper = _database.OpenConnection())
             {
-                mapper.Execute(@"DELETE FROM MetadataFiles
-                                     WHERE Id IN (
-                                     SELECT MetadataFiles.Id FROM MetadataFiles
-                                     LEFT OUTER JOIN Albums
-                                     ON MetadataFiles.AlbumId = Albums.Id
-                                     WHERE MetadataFiles.AlbumId > 0
-                                     AND Albums.Id IS NULL)");
+                mapper.Execute(@"DELETE FROM ""MetadataFiles""
+                                     WHERE ""Id"" IN (
+                                     SELECT ""MetadataFiles"".""Id"" FROM ""MetadataFiles""
+                                     LEFT OUTER JOIN ""Albums""
+                                     ON ""MetadataFiles"".""AlbumId"" = ""Albums"".""Id""
+                                     WHERE ""MetadataFiles"".""AlbumId"" > 0
+                                     AND ""Albums"".""Id"" IS NULL)");
             }
         }
 
@@ -52,13 +52,13 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
         {
             using (var mapper = _database.OpenConnection())
             {
-                mapper.Execute(@"DELETE FROM MetadataFiles
-                                     WHERE Id IN (
-                                     SELECT MetadataFiles.Id FROM MetadataFiles
-                                     LEFT OUTER JOIN TrackFiles
-                                     ON MetadataFiles.TrackFileId = TrackFiles.Id
-                                     WHERE MetadataFiles.TrackFileId > 0
-                                     AND TrackFiles.Id IS NULL)");
+                mapper.Execute(@"DELETE FROM ""MetadataFiles""
+                                     WHERE ""Id"" IN (
+                                     SELECT ""MetadataFiles"".""Id"" FROM ""MetadataFiles""
+                                     LEFT OUTER JOIN ""TrackFiles""
+                                     ON ""MetadataFiles"".""TrackFileId"" = ""TrackFiles"".""Id""
+                                     WHERE ""MetadataFiles"".""TrackFileId"" > 0
+                                     AND ""TrackFiles"".""Id"" IS NULL)");
             }
         }
 
@@ -66,11 +66,11 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
         {
             using (var mapper = _database.OpenConnection())
             {
-                mapper.Execute(@"DELETE FROM MetadataFiles
-                                     WHERE Id IN (
-                                     SELECT Id FROM MetadataFiles
-                                     WHERE Type IN (4, 6)
-                                     AND AlbumId = 0)");
+                mapper.Execute(@"DELETE FROM ""MetadataFiles""
+                                     WHERE ""Id"" IN (
+                                     SELECT ""Id"" FROM ""MetadataFiles""
+                                     WHERE ""Type"" IN (4, 6)
+                                     AND ""AlbumId"" = 0)");
             }
         }
 
@@ -78,11 +78,11 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
         {
             using (var mapper = _database.OpenConnection())
             {
-                mapper.Execute(@"DELETE FROM MetadataFiles
-                                     WHERE Id IN (
-                                     SELECT Id FROM MetadataFiles
-                                     WHERE Type IN (2, 5)
-                                     AND TrackFileId = 0)");
+                mapper.Execute(@"DELETE FROM ""MetadataFiles""
+                                     WHERE ""Id"" IN (
+                                     SELECT ""Id"" FROM ""MetadataFiles""
+                                     WHERE ""Type"" IN (2, 5)
+                                     AND ""TrackFileId"" = 0)");
             }
         }
     }

src/NzbDrone.Core/Housekeeping/Housekeepers/CleanupOrphanedPendingReleases.cs
@@ -16,12 +16,12 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
         {
             using (var mapper = _database.OpenConnection())
             {
-                mapper.Execute(@"DELETE FROM PendingReleases
-                                     WHERE Id IN (
-                                     SELECT PendingReleases.Id FROM PendingReleases
-                                     LEFT OUTER JOIN Artists
-                                     ON PendingReleases.ArtistId = Artists.Id
-                                     WHERE Artists.Id IS NULL)");
+                mapper.Execute(@"DELETE FROM ""PendingReleases""
+                                     WHERE ""Id"" IN (
+                                     SELECT ""PendingReleases"".""Id"" FROM ""PendingReleases""
+                                     LEFT OUTER JOIN ""Artists""
+                                     ON ""PendingReleases"".""ArtistId"" = ""Artists"".""Id""
+                                     WHERE ""Artists"".""Id"" IS NULL)");
             }
         }
     }

Sportarr/Sportarr

src/NzbDrone.Core/Housekeeping/Housekeepers/CleanupOrphanedMetadataFiles.cs
@@ -22,34 +22,34 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
         private void DeleteOrphanedBySeries()
         {
             using var mapper = _database.OpenConnection();
-            mapper.Execute(@"DELETE FROM MetadataFiles
-                             WHERE Id IN (
-                             SELECT MetadataFiles.Id FROM MetadataFiles
-                             LEFT OUTER JOIN Series
-                             ON MetadataFiles.SeriesId = Series.Id
-                             WHERE Series.Id IS NULL)");
+            mapper.Execute(@"DELETE FROM ""MetadataFiles""
+                                     WHERE ""Id"" IN (
+                                     SELECT ""MetadataFiles"".""Id"" FROM ""MetadataFiles""
+                                     LEFT OUTER JOIN ""Series""
+                                     ON ""MetadataFiles"".""SeriesId"" = ""Series"".""Id""
+                                     WHERE ""Series"".""Id"" IS NULL)");
         }
 
         private void DeleteOrphanedByEpisodeFile()
         {
             using var mapper = _database.OpenConnection();
-            mapper.Execute(@"DELETE FROM MetadataFiles
-                             WHERE Id IN (
-                             SELECT MetadataFiles.Id FROM MetadataFiles
-                             LEFT OUTER JOIN EpisodeFiles
-                             ON MetadataFiles.EpisodeFileId = EpisodeFiles.Id
-                             WHERE MetadataFiles.EpisodeFileId > 0
-                             AND EpisodeFiles.Id IS NULL)");
+            mapper.Execute(@"DELETE FROM ""MetadataFiles""
+                                     WHERE ""Id"" IN (
+                                     SELECT ""MetadataFiles"".""Id"" FROM ""MetadataFiles""
+                                     LEFT OUTER JOIN ""EpisodeFiles""
+                                     ON ""MetadataFiles"".""EpisodeFileId"" = ""EpisodeFiles"".""Id""
+                                     WHERE ""MetadataFiles"".""EpisodeFileId"" > 0
+                                     AND ""EpisodeFiles"".""Id"" IS NULL)");
         }
 
         private void DeleteWhereEpisodeFileIsZero()
         {
             using var mapper = _database.OpenConnection();
-            mapper.Execute(@"DELETE FROM MetadataFiles
-                             WHERE Id IN (
-                             SELECT Id FROM MetadataFiles
-                             WHERE Type IN (2, 5)
-                             AND EpisodeFileId = 0)");
+            mapper.Execute(@"DELETE FROM ""MetadataFiles""
+                                     WHERE ""Id"" IN (
+                                     SELECT ""Id"" FROM ""MetadataFiles""
+                                     WHERE ""Type"" IN (2, 5)
+                                     AND ""EpisodeFileId"" = 0)");
         }
     }
 }

src/NzbDrone.Core/Housekeeping/Housekeepers/CleanupOrphanedPendingReleases.cs
@@ -15,12 +15,12 @@ namespace NzbDrone.Core.Housekeeping.Housekeepers
         public void Clean()
         {
             using var mapper = _database.OpenConnection();
-            mapper.Execute(@"DELETE FROM PendingReleases
-                             WHERE Id IN (
-                             SELECT PendingReleases.Id FROM PendingReleases
-                             LEFT OUTER JOIN Series
-                             ON PendingReleases.SeriesId = Series.Id
-                             WHERE Series.Id IS NULL)");
+            mapper.Execute(@"DELETE FROM ""PendingReleases""
+                                     WHERE ""Id"" IN (
+                                     SELECT ""PendingReleases"".""Id"" FROM ""PendingReleases""
+                                     LEFT OUTER JOIN ""Series""
+                                     ON ""PendingReleases"".""SeriesId"" = ""Series"".""Id""
+                                     WHERE ""Series"".""Id"" IS NULL)");
         }
     }
 }