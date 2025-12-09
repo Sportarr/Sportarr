8a59da6e37a86f8c134e6106d12ed0652b07fcfa
Fightarr <unmannedmediaplex@gmail.com>
2025-12-08 22:41:05 -0500

    fix: Add migration for TrashSyncSettings column

---
+++ src/Migrations/20251209034050_AddTrashSyncSettingsColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sportarr.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTrashSyncSettingsColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TrashSyncSettings",
+                table: "AppSettings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TrashSyncSettings",
+                table: "AppSettings");
+        }
+    }
+}
